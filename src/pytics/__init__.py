85ba7922b1affead1bfc86ae921dfb433db97aa1
Hans Meershoek <meershoek@gmail.com>
2025-04-06 22:48:33 +0200

    chore: bump version to 1.1.5

--- src/pytics/__init__.py
+++ src/pytics/__init__.py
@@ -4,5 +4,5 @@ Data Profiler - Interactive data profiling library for Python notebooks
 
 from .profiler import profile, compare
 
-__version__ = "1.0.0"
+__version__ = "1.1.5"
 __all__ = ["profile", "compare"] 
